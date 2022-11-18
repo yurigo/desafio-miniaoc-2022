12ea36ede19644d3550a1d3d6bc163c16339ec56
Yuri <ygonzoo@gmail.com>
2022-11-18 21:51:14 +0100

    bugfix rainbow separator (f*** ios)

--- src/RainbowSeparator.vue
+++ src/RainbowSeparator.vue
@@ -1,22 +1,87 @@
 <template>
-    <div></div>
+    <div class="rainbow">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 </template>
 
 <style scoped>
-div {
+div.rainbow {
+    position: relative;
+    width: 200px;
+    height: 100px;
     margin: 4em auto 4em;
 }
 
-div::after,
+div.rainbow>div {
+    position: absolute;
+    top: 0px;
+    width: 200px;
+    height: 100px;
+    border-radius: 110px 110px 0 0;
+    /* box-shadow:
+        0px 0px 0 10px rgb(243, 19, 19) inset,
+        0px 0px 0 20px rgb(255, 145, 0) inset,
+        0px 0px 0 30px rgb(252, 223, 3) inset,
+        0px 0px 0 40px rgb(5, 232, 5) inset,
+        0px 0px 0 50px rgb(59, 135, 249) inset,
+        0px 0px 0 60px rgb(196, 98, 226) inset,
+        0px 0px 0 70px rgb(103, 28, 190) inset; */
+
+}
+
+div.rainbow>div:nth-child(7) {
+    border: 10px solid rgb(243, 19, 19);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(6) {
+    border: 20px solid rgb(255, 145, 0);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(5) {
+    border: 30px solid rgb(252, 223, 3);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(4) {
+    border: 40px solid rgb(5, 232, 5);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(3) {
+    border: 50px solid rgb(59, 135, 249);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(2) {
+    border: 60px solid rgb(196, 98, 226);
+    border-bottom: 0;
+}
+
+div.rainbow>div:nth-child(1) {
+    border: 70px solid rgb(103, 28, 190);
+    border-bottom: 0;
+}
+
+/* div::after,
 div::before {
     display: inline-block;
     content: " ";
     width: 100px;
     height: 100px;
-}
+    -webkit-appearance: none;
+    appearance: none;
+} */
 
-div::before {
-    border-top-left-radius: 150%;
+/* div::before {
+    border-radius: 100% 0 0 0;
     box-shadow:
         5px 5px 0 5px rgb(243, 19, 19) inset,
         10px 10px 0 10px rgb(255, 145, 0) inset,
@@ -25,11 +90,10 @@ div::before {
         25px 25px 0 25px rgb(59, 135, 249) inset,
         30px 30px 0 30px rgb(196, 98, 226) inset,
         35px 35px 0 35px rgb(103, 28, 190) inset;
-
 }
 
 div::after {
-    border-top-right-radius: 100%;
+    border-top-right-radius: 100px;
     box-shadow:
         -5px 5px 0 5px rgb(243, 19, 19) inset,
         -10px 10px 0 10px rgb(255, 145, 0) inset,
@@ -38,5 +102,5 @@ div::after {
         -25px 25px 0 25px rgb(59, 135, 249) inset,
         -30px 30px 0 30px rgb(196, 98, 226) inset,
         -35px 35px 0 35px rgb(103, 28, 190) inset;
-}
+} */
 </style>
